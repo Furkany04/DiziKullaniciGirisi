3d55a2b5cfdde007ad72a0d43629afcdd4206385
Furkany04 <furkan.yildiz.k@outlook.com>
2023-01-26 22:25:08 +0300

    KullaniciGirisi

---
+++ çiftboyutludizikullanıcıgiriş/çiftboyutludizikullanıcıgiriş/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace çiftboyutludizikullanıcıgiriş
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.adtxt = new System.Windows.Forms.TextBox();
+            this.girisbtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sifretxt = new System.Windows.Forms.TextBox();
+            this.bilgitxt = new System.Windows.Forms.Label();
+            this.kaydolbtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.idtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(309, 192);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "kullanıcı adı : ";
+            // 
+            // adtxt
+            // 
+            this.adtxt.Location = new System.Drawing.Point(477, 193);
+            this.adtxt.Name = "adtxt";
+            this.adtxt.Size = new System.Drawing.Size(100, 27);
+            this.adtxt.TabIndex = 1;
+            // 
+            // girisbtn
+            // 
+            this.girisbtn.Location = new System.Drawing.Point(659, 184);
+            this.girisbtn.Name = "girisbtn";
+            this.girisbtn.Size = new System.Drawing.Size(109, 44);
+            this.girisbtn.TabIndex = 2;
+            this.girisbtn.Text = "Giriş yap";
+            this.girisbtn.UseVisualStyleBackColor = true;
+            this.girisbtn.Click += new System.EventHandler(this.girisbtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(309, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "şifre :";
+            // 
+            // sifretxt
+            // 
+            this.sifretxt.Location = new System.Drawing.Point(477, 241);
+            this.sifretxt.Name = "sifretxt";
+            this.sifretxt.Size = new System.Drawing.Size(100, 27);
+            this.sifretxt.TabIndex = 1;
+            // 
+            // bilgitxt
+            // 
+            this.bilgitxt.AutoSize = true;
+            this.bilgitxt.Location = new System.Drawing.Point(283, 447);
+            this.bilgitxt.Name = "bilgitxt";
+            this.bilgitxt.Size = new System.Drawing.Size(44, 22);
+            this.bilgitxt.TabIndex = 0;
+            this.bilgitxt.Text = "Bilgi";
+            // 
+            // kaydolbtn
+            // 
+            this.kaydolbtn.Location = new System.Drawing.Point(395, 340);
+            this.kaydolbtn.Name = "kaydolbtn";
+            this.kaydolbtn.Size = new System.Drawing.Size(109, 44);
+            this.kaydolbtn.TabIndex = 2;
+            this.kaydolbtn.Text = "Kaydol";
+            this.kaydolbtn.UseVisualStyleBackColor = true;
+            this.kaydolbtn.Visible = false;
+            this.kaydolbtn.Click += new System.EventHandler(this.kaydolbtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(309, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "id  : ";
+            // 
+            // idtxt
+            // 
+            this.idtxt.Location = new System.Drawing.Point(477, 147);
+            this.idtxt.Name = "idtxt";
+            this.idtxt.Size = new System.Drawing.Size(100, 27);
+            this.idtxt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 537);
+            this.Controls.Add(this.kaydolbtn);
+            this.Controls.Add(this.girisbtn);
+            this.Controls.Add(this.sifretxt);
+            this.Controls.Add(this.idtxt);
+            this.Controls.Add(this.adtxt);
+            this.Controls.Add(this.bilgitxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox adtxt;
+        private System.Windows.Forms.Button girisbtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox sifretxt;
+        private System.Windows.Forms.Label bilgitxt;
+        private System.Windows.Forms.Button kaydolbtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox idtxt;
+    }
+}
+
---
+++ çiftboyutludizikullanıcıgiriş/çiftboyutludizikullanıcıgiriş/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace çiftboyutludizikullanıcıgiriş
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[,] kullanıcı = new string[2,3];
+        
+        
+        
+        int i = 0, j = 0;
+
+        
+
+        private void girisbtn_Click(object sender, EventArgs e)
+        {
+            
+          
+
+            if (kullanıcı[0, 0] == idtxt.Text && kullanıcı[0, 1] == adtxt.Text && kullanıcı[0, 2] == sifretxt.Text)
+            {
+                bilgitxt.Text = "giriş başarılı";
+            }
+            else if (kullanıcı[1, 0] == idtxt.Text && kullanıcı[1, 1] == adtxt.Text && kullanıcı[1, 2] == sifretxt.Text)
+            {
+                bilgitxt.Text = "giriş başarılı";
+            }
+            else
+            {
+                bilgitxt.Text = "kullanıcı bulunamadı, kaydolmak için kaydol butonuna basınız!";
+                kaydolbtn.Visible= true;
+            }
+
+
+
+
+
+        }
+
+        private void kaydolbtn_Click(object sender, EventArgs e)
+        {
+            kaydolbtn.Visible= false;
+            if (i < 2 )
+            {
+                kullanıcı[i, j] = idtxt.Text;
+                j++;
+                kullanıcı[i, j] = adtxt.Text;
+                j++;
+                kullanıcı[i, j] = sifretxt.Text;
+                i++;
+                
+                j = 0;
+                bilgitxt.Text = "kayıt başarılı";
+            }
+            else
+            {
+                bilgitxt.Text = "çok fazla kullanıcı kaydolamaz";
+            }
+
+        }
+    }
+}
